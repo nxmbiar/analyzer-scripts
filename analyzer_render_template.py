94747235ceaf45cb8ef91383a5b824766fee4ec1
Anant M Nambiar <anant.nambiar@gmail.com>
2021-08-12 13:02:39 +0530

    Update analyzer_render_template.py

--- analyzer_render_template.py
+++ analyzer_render_template.py
@@ -8,11 +8,11 @@ env = Environment(loader=file_loader)
 
 template = env.get_template('ANALYZER_ISSUE_TEMPLATE.md')
 
-with open(path.join('.github','test.json')) as f:
+with open(path.join(sys.argv[1],'.github','failed_issues.json')) as f:
     issues = loads(f.read())
 # issues = loads(sys.argv[1])
 
-output = template.render(issues=issues)
+output = template.render(failed_issues=issues)
 
-with open(path.join(path.dirname(__file__),'ISSUE.md'),'w', encoding='utf-8') as f:
+with open(path.join(path.dirname(__file__),'ISSUE.md'),'w', encoding="utf-8") as f:
     f.write(output)
